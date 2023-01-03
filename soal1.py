b61c3c689dd2f51f70d138f7a33f83bd38bbb2d7
elizfernando <118380082+elizfernando@users.noreply.github.com>
2023-01-03 12:24:12 +0700

    Add files via upload

---
+++ soal1.py
@@ -0,0 +1,32 @@
+#1
+print("*"*6, "Kredit Keaktifan Mahasiswa", "*"*5)
+print("(Student Activities Credit)")
+print("1. Menambah Kegiatan")
+print("2. Menampilkan Jumlah Poin")
+print("3. Keluar")
+print("-"*28)
+yg = int(input("Silahkan masukan pilihan yang anda:"))
+
+if yg == 1:
+    sm = input("Nama Kegiatan:")
+    
+
+jy = int(input("Tanggal Kegiatan:"))
+sk = int(input("Pilihan Kategori Kegiatan:"))
+print("- Prestasi")
+print("- Organisasi")
+print("- Kepanitiaan")
+print("- Rekognisi")
+yk = int(input("Masukan Kategori Kegiatan:"))
+
+print("Kegiatan berhasil ditambahkan.")
+
+print("8"*6, "Kredit Keaktifan Mahasiswa, "*"*5")
+print("(Student Activities Credit)")
+print("1. Menambahkan Kegiatan")
+print("2. Menampilkan Jumlah Poin")
+print("3. Keluar")
+print("-"*28)
+uk = int(input("Silahkan masukan pilihan yang Anda:"))
+
+
